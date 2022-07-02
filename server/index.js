d1d34a048128805aa5e9dcb7efd004b746842875
Cesar Sepulveda <cesarsg.dev@gmail.com>
2022-07-01 23:04:15 -0500

    Express initiation

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: `This is the main route` });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
